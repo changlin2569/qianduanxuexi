30f8b7e001cda16de0211628f3f5ef397bb927e4
lichanglinshan <1119785353@qq.com>
2020-09-22 22:20:15 +0800

    node第三方模块

--- 前后端交互 Node+Gulp/Node/nodehello world.js
+++ 前后端交互 Node+Gulp/Node/nodehello world.js
@@ -2,10 +2,12 @@ var x = 'hello world';
 console.log(x);
 
 function fn() {
-    console.log(函数调用);
+    console.log('函数调用');
 }
 fn();
 
 for (var i = 0; i < 3; i++) {
     console.log(i);
 }
+
+console.log('我已经被修改了');
