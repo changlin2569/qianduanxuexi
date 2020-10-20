12d017a83f2da095675270bcf7a0bbe4187e85e8
lichanglinshan <1119785353@qq.com>
2020-10-20 22:34:24 +0800

    ajax函数封装

--- 前后端交互 Node+Gulp/Ajax/app.js
+++ 前后端交互 Node+Gulp/Ajax/app.js
@@ -2,9 +2,16 @@
 const express = require('express');
 // 路径处理模块
 const path = require('path');
+//接收post请求参数
+const bodyParser = require('body-parser');
 // 创建文本服务器
 const app = express();
-
+// 文件操作
+const fs = require('fs');
+// 解析url参数
+app.use(bodyParser.urlencoded());
+// 解析json数据
+app.use(bodyParser.json());
 // 静态资源访问服务功能
 app.use(express.static(path.join(__dirname,'public')));
 
@@ -16,6 +23,29 @@ app.get('/first',(req,res) => {
 
 app.get('/responseData',(req,res) => {
     res.send({'name':'linuan'})
+})
+
+app.get('/get',(req,res) => {
+    res.send(req.query)
+    // req.query保存着请求的参数
+})
+
+app.post('/post',(req,res) => {
+    res.send(req.body)
+})
+
+app.post('/json',(req,res) => {
+    res.send(req.body)
+})
+
+app.get('/error',(req,res) => {
+    res.status(400).send()
+})
+
+app.get('/cache',(req,res) => {
+    fs.readFile('./test.txt',(err,result) => {
+        res.send(result);
+    })
 })
 // 监听端口
 app.listen(3000);
