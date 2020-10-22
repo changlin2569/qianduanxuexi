bd180f9178fd64620622e63b5e8c0c2770d6ff99
lichanglinshan <1119785353@qq.com>
2020-10-22 21:22:08 +0800

    ajax

--- 前后端交互 Node+Gulp/Ajax/app.js
+++ 前后端交互 Node+Gulp/Ajax/app.js
@@ -16,52 +16,135 @@ app.use(bodyParser.urlencoded());
 // 解析json数据
 app.use(bodyParser.json());
 // 静态资源访问服务功能
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // 添加路由
-app.get('/first',(req,res) => {
+app.get('/first', (req, res) => {
     res.send('hello ajax');
     // res.send(path.join(__dirname,'public'))
 })
 
-app.get('/responseData',(req,res) => {
-    res.send({'name':'linuan'})
+app.get('/responseData', (req, res) => {
+    res.send({
+        'name': 'linuan'
+    })
 })
 
-app.get('/get',(req,res) => {
+app.get('/get', (req, res) => {
     res.send(req.query)
     // req.query保存着请求的参数
 })
 
-app.post('/post',(req,res) => {
+app.post('/post', (req, res) => {
     res.send(req.body)
 })
 
-app.post('/json',(req,res) => {
+app.post('/json', (req, res) => {
     res.send(req.body)
 })
 
-app.get('/error',(req,res) => {
+app.get('/error', (req, res) => {
     res.status(400).send()
 })
 
-app.get('/cache',(req,res) => {
-    fs.readFile('./test.txt',(err,result) => {
+app.get('/cache', (req, res) => {
+    fs.readFile('./test.txt', (err, result) => {
         res.send(result);
     })
 })
 
 // 验证邮箱地址唯一性
-app.get('/verifyEmailAdress',(req,res) => {
+app.get('/verifyEmailAdress', (req, res) => {
     // res.send(url.parse(req.url,true))
-    let params = url.parse(req.url,true)
-    if(params.query.email == 'itheima@qq.com'){
-        res.status(400).send({'message': '您输入的邮箱已被占用'})
-    }else{
-        res.send({'message': '邮箱输入正确'})
+    let params = url.parse(req.url, true)
+    if (params.query.email == 'itheima@qq.com') {
+        res.status(400).send({
+            'message': '您输入的邮箱已被占用'
+        })
+    } else {
+        res.send({
+            'message': '邮箱输入正确'
+        })
+    }
+})
+// 搜索框自动提示
+app.get('/searchAutoPrompt', (req, res) => {
+    let params = url.parse(req.url, true);
+    // res.send(params)
+    if (params.query.key == '黑马') {
+        res.send(['黑马程序员', '黑马程序员官网', '黑马程序员哔哩哔哩'])
     }
 })
 
+// 省市区三级联动
+app.get('/province', (req, res) => {
+    res.send([{
+            id: '001',
+            name: '陕西省'
+        }, {
+            id: '002',
+            name: '四川省'
+        },
+        {
+            id: '003',
+            name: '河北省'
+        }, {
+            id: '004',
+            name: '浙江省'
+        }
+    ])
+})
+
+app.get('/cities',(req,res) => {
+    let params = url.parse(req.url,true);
+    // res.send(params);
+    // res.send(req.url);
+    if(params.query.id == '001'){
+        res.send([{
+            id: '100',
+            name: '西安市'
+        },{
+            id: '101',
+            name: '安康市'
+        },{
+            id: '102',
+            name: '延安市'
+        }])
+    }else if(params.query.id == '002'){
+        res.send([{
+            id: '200',
+            name: '成都市'
+        },{
+            id: '201',
+            name: '内江市'
+        },{
+            id: '202',
+            name: '乐山市'
+        }])
+    }else if(params.query.id == '003'){
+        res.send([{
+            id: '300',
+            name: '邯郸市'
+        },{
+            id: '301',
+            name: '邢台市'
+        },{
+            id: '302',
+            name: '张家口市'
+        }])
+    }else if(params.query.id == '004'){
+        res.send([{
+            id: '400',
+            name: '杭州市'
+        },{
+            id: '401',
+            name: '宁波市'
+        },{
+            id: '402',
+            name: '嘉兴市'
+        }])
+    }
+})
 // 监听端口
 app.listen(3000);
 
