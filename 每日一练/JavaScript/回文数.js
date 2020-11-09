47b3fd3e5f932239a61f134b04976f5dcc9c99f4
lichanglinshan <1119785353@qq.com>
2020-11-09 22:10:56 +0800

    每日一练

--- 每日一练/JavaScript/回文数.js
+++ 每日一练/JavaScript/回文数.js
@@ -1,7 +1,8 @@
 function isPalind(argue) {
     let count = argue.toString().split('').reverse().join('');
+    console.log(count);
     return argue.toString() == count;
 }
 
 let x = 121;
-console.log(isPalind(x));;
+console.log(isPalind(x));
