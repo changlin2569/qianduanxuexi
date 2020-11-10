261b9aefa96e39d7d69d9c53c0ddd4867c785b70
lichanglinshan <1119785353@qq.com>
2020-11-10 22:04:05 +0800

    上下文

---
+++ 每日一练/JavaScript/数组中对象归并.js
@@ -0,0 +1,25 @@
+//实现以下输出
+// let inArr = [
+//     {
+//         company: 'COM1',
+//         customer: 'CUS1'
+//     },
+//     {
+//         company: 'COM1',
+//         customer: 'CUS2',
+//     },
+//     {
+//         company: 'COM2',
+//         customer: 'CUS3'
+//     }
+//   ]
+//   let outArr = [
+//     {
+//         company: 'COM1',
+//         custome: ['CUS1', 'CUS2']
+//     },
+//     {
+//         company: 'COM2',
+//         custome: ['CUS3']
+//     }
+//   ]
