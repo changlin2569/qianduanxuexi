7a26c6365db12c05274d49a00b1c31bf76715727
lichanglinshan <1119785353@qq.com>
2021-03-26 22:00:20 +0800

    fakeVue

--- 每日一练/JavaScript/fakeVue.js
+++ 每日一练/JavaScript/fakeVue.js
@@ -1,3 +1,59 @@
+// 初始化视图对象
+const compileUtil = {
+    // 根据属性值获取data中的值
+    getDataValue(attrValue, vm) {
+        // 拆分例如 person.name 这样的属性值
+        const data = vm.$data;
+        const attrValueArr = attrValue.split('.');
+        return attrValueArr.reduce((prev, item) => {
+            prev = prev[item];
+            return prev;
+        }, data);
+    },
+    text(node, value, vm) {
+        // let current = data;
+        // valueArr.forEach(item => {
+        //     current = current[item];
+        //     console.log(current);
+        // })
+        let current;
+        if (/\{\{(.+?)\}\}/.test(value)) {
+            current = value.replace(/\{\{(.+?)\}\}/g, (...args) => {
+                return this.getDataValue(args[1], vm);
+            });
+        } else {
+            current = this.getDataValue(value, vm);
+            // console.log(current);
+            // node.innerHTML = current;
+        }
+        this.Updater.updateText(node, current);
+    },
+    html(node, value, vm) {
+        const current = this.getDataValue(value, vm);
+        // node.innerHTML = current;
+        this.Updater.updateHtml(node, current);
+    },
+    model(node, value, vm) {
+        const current = this.getDataValue(value, vm);
+        // node.value = current;
+        this.Updater.updateModel(node, current)
+    },
+    on(node, value, vm, eventName) {
+        node.addEventListener(eventName, vm.$options.methods[value].bind(vm));
+    },
+    Updater: {
+        updateText(node, current) {
+            node.nodeValue = current;
+        },
+        updateHtml(node, current) {
+            node.innerHTML = current;
+        },
+        updateModel(node, current) {
+            node.value = current;
+        }
+    }
+}
+
 class fakeVue {
     constructor(options) {
         this.$el = options.el;
@@ -16,9 +72,13 @@ class fakeVue {
 class Compile {
     constructor(el, vm) {
         this.el = this.isElementNode(el) ? el : document.querySelector(el);
+        this.vm = vm;
         const fEl = this.creatFragment(this.el);
-        console.log(this.compile(fEl));
-        // this.el.appendChild(fEl);
+        // 解析文档碎片
+        this.compile(fEl);
+        // console.log(this.compile(fEl));
+        // 将文档碎片插入到dom树中
+        this.el.appendChild(fEl);
     }
     //  判断是否为元素节点
     isElementNode(node) {
@@ -42,15 +102,34 @@ class Compile {
             }
             if (item.childNodes && item.childNodes.length > 0) {
                 this.compile(item);
-            } 
+            }
         })
     }
     // 解析元素节点
     compileElement(node) {
-        console.log(node.attributes);
+        [...node.attributes].forEach(item => {
+            if (this.isDirective(item.name)) {
+                // 解构属性的键值
+                const { name, value } = item;
+                const [, directive] = name.split('-');
+                const [directiveName, eventName] = directive.split(':');
+                // 调用对应的updater方法更新视图
+                compileUtil[directiveName](node, value, this.vm, eventName);
+                // 移除  v- 属性
+                node.removeAttribute('v-' + directive)
+            }
+        })
     }
     // 解析文本节点
-    compileText(text) {
-
+    compileText(node) {
+        // console.log(node.nodeValue);
+        const content = node.nodeValue;
+        if (/\{\{(.+?)\}\}/.test(content)) {
+            compileUtil['text'](node, content, this.vm);
+        }
+    }
+    // 判断是否为vue属性
+    isDirective(name) {
+        return name.startsWith('v-');
     }
 }
