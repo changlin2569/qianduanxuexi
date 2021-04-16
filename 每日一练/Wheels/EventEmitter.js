b83b583e76ce2cd9975f0b63f8b7b3c0cca0d744
lichanglinshan <1119785353@qq.com>
2021-04-16 21:28:01 +0800

    EventEmitter

--- 每日一练/Wheels/EventEmitter.js
+++ 每日一练/Wheels/EventEmitter.js
@@ -1,45 +1,87 @@
 (function() {
-    EventEmitter = function() {
+    const _this = ((typeof window === 'object') && window) || ((typeof Global === 'object') && global);
+    _this.EventEmitter = function() {
         this.events = {}
+    }
+
+    let Util = {
+        isFunction(fn) {
+            if (typeof fn === 'function') {
+                return true
+            } else if (typeof fn === 'object') {
+                return typeof fn.listener === 'function'
+            }
+            return false
+        },
+
+        indexOf(objFn, eventArr) {
+            let _index = -1;
+            let current = (typeof objFn === 'function') ? objFn : objFn.listener 
+            for (let i = 0; i < eventArr.length; i++) {
+                if (eventArr[i].listener === current) {
+                    _index = i;
+                    break;
+                }
+            }
+            return _index
+        },
+
+        isError(eventName, listener) {
+            if (!eventName || !listener) {
+                throw new Error('Please pass parameters')
+            } else if (!Util.isFunction(listener)) {
+                throw new Error('listener must be a function');
+            }
+        }
     }
     
     EventEmitter.prototype.on = function(eventName, listener) {
-        if (!this.events[eventName]) {
-            this.events[eventName] = [];
+        Util.isError(eventName, listener);
+        this.events[eventName] = this.events[eventName] || [];
+        let once = typeof listener === 'object' ? true : false;
+        if (Util.indexOf(listener, this.events[eventName]) === -1) {
+            if (!once) {
+                this.events[eventName].push({
+                    listener,
+                    once
+                });
+            } else {
+                this.events[eventName].push(listener);
+            }
         }
-        this.events[eventName].push(listener);
         return this;
     }
     
     EventEmitter.prototype.once = function (eventName, listener) {
-        if (!this.events[eventName]) {
-            this.events[eventName] = [];
-        }
-        listener.once = true;
-        this.events[eventName].push(listener);
+        Util.isError(eventName, listener);
+        let once = true;
+        this.on(eventName, {
+            listener,
+            once
+        })
         return this;
     }
     
     EventEmitter.prototype.emit = function(eventName, args) {
-        if (!this.events[eventName]) {
-            return 'no eventName'
+        if (!eventName) {
+            throw new Error('Please pass parameters')
+        } else if (!this.events[eventName]) {
+            return
         }
         for (let i = 0; i < this.events[eventName].length; i++) {
-            // console.log(this.events[eventName][i]);
-            this.events[eventName][i](...args);
-            if (this.events[eventName][i].once) {
-                this.events[eventName].splice(i,1);
-                i--
+            let { listener, once } = this.events[eventName][i];
+            listener.apply(this, args);
+            if (once) {
+                this.events[eventName].splice(i, 1);
+                i--;
             }
         }
         return this
     }
     
     EventEmitter.prototype.off = function (eventName, listener) {
-        if (!this.events[eventName]) {
-            return 'no eventName'
-        }
-        this.events[eventName] = this.events[eventName].filter(item => item !== listener)
+        Util.isError(eventName, listener);
+        this.events[eventName] = this.events[eventName].filter(item => item.listener !== listener)
         return this;
     }
     
---
+++ 每日一练/Wheels/ProgressIndicator.js
@@ -0,0 +1,16 @@
+(function () {
+    ProgressIndicator = function (color = {
+        color: "#e74c3c"
+    }) {
+        this.barStyle = color;
+        this.el = document.querySelector('body');
+    }
+
+    ProgressIndicator.prototype.on = function(event) {
+
+    }
+
+    ProgressIndicator.prototype.once = function(event) {
+
+    }
+})()
