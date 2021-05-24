8960e372a4b6b4ecada48953f2c9f4e24c08f27a
lichanglinshan <1119785353@qq.com>
2021-05-24 21:17:46 +0800

    每日一练

--- 每日一练/JavaScript/大数相加.js
+++ 每日一练/JavaScript/大数相加.js
@@ -20,7 +20,35 @@ function addLargeNumber(num1, num2) {
         arr[i] = item >= 10 ? item % 10 : item;
         item = item >= 10 ? 1 : 0;
     }
+    if (item) {
+        arr.unshift(item);
+    }
     return arr.join('').replace(/0/, '');
 }
 
+function add(num1, num2) {
+    num1 = ('' + num1).split('');
+    num2 = ('' + num2).split('');
+    const arr = [];
+    let item1, item2, item = 0;
+    while(num1.length || num2.length) {
+        item1 = +num1.pop() || 0;
+        item2 = +num2.pop() || 0;
+        item = item1 + item2 + item;
+        if (item >= 10) {
+            arr.push(item % 10);
+            item = 1;
+        } else {
+            arr.push(item);
+            item = 0;
+        }
+    }
+    if (item) {
+        arr.push(item);
+    }
+    arr.reverse();
+    return arr.join('')
+}
+
 console.log(addLargeNumber('1568415132154684984', '2012'));
+console.log(add('1568415132154684984', '2012'))
