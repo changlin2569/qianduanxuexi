7f9eb46bd27248f9e0c22ce5e5b7adcfcd596e66
lichanglinshan <1119785353@qq.com>
2021-11-27 21:52:50 +0800

    practice

--- 每日一练/JavaScript/getArrayDeep.js
+++ 每日一练/JavaScript/getArrayDeep.js
@@ -1,29 +1,51 @@
-const arr = [1, 2, 3, [4, 5, [6, 7]]]
-
 function getArrayDeep(arr) {
-    if (!Array.isArray(arr)) {
-        return
+    if (!arr.length) {
+        return 0
     }
+    let queue = [].concat(arr)
     let deep = 0
-    let newArr = [].concat(arr)
     while (true) {
-        let flag = newArr.some(item => Array.isArray(item))
+        let flag = queue.some(item => Array.isArray(item))
         if (flag) {
-            newArr = [].concat(...newArr)
             deep++
+            queue = [].concat(...queue)
         } else {
             return deep
         }
     }
 }
 
-console.log(getArrayDeep([0, [2],
-    [2, [3]]
-]));
-console.log(getArrayDeep([
-    [
-        [
-            []
-        ]
-    ]
-]));
+function getArrayDeep(arr, deep = 0) {
+    // 保存每一层的深度快照，不然会随着迭代的次数导致 deep不准确
+    let res = deep
+
+    for (let i = 0; i < arr.length; i++) {
+        if (Array.isArray(arr[i])) {
+            deep = Math.max(deep, getArrayDeep(arr[i], res + 1))
+        }
+    }
+
+    return deep
+}
+
+function getArrayDeep(arr) {
+    // 深度值设置为 -1 的原因是因为 条件判断为当前值是否为数组
+    let deep = -1
+
+    if (!Array.isArray(arr)) {
+        return deep
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        deep = Math.max(deep, getArrayDeep(arr[i]))
+    }
+
+    return deep + 1
+}
+
+let len1 = getArrayDeep([1, 2, [3, [1, [0]]]]); // 返回 3
+let len2 = getArrayDeep([]); // 返回 0
+let len3 = getArrayDeep([[[[]]]]); // 返回 3
+let len4 = getArrayDeep([[0], [2], [2, [3]]]); // 返回 2
+
+console.log(len1, len2, len3, len4);
